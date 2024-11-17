f36d82f3ea5462cea3a6416c5b772c7d40b96c05
Walter Spurgiasz <walter@conciseusa.com>
2024-11-16 20:37:01 -0800

    Progress check in

---
+++ DinVesa/DinVesa.ino
@@ -0,0 +1,158 @@
+/*
+
+  HelloWorld.ino
+
+  Universal 8bit Graphics Library (https://github.com/olikraus/u8g2/)
+
+  Copyright (c) 2016, olikraus@gmail.com
+  All rights reserved.
+
+  Redistribution and use in source and binary forms, with or without modification, 
+  are permitted provided that the following conditions are met:
+
+  * Redistributions of source code must retain the above copyright notice, this list 
+    of conditions and the following disclaimer.
+    
+  * Redistributions in binary form must reproduce the above copyright notice, this 
+    list of conditions and the following disclaimer in the documentation and/or other 
+    materials provided with the distribution.
+
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND 
+  CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, 
+  INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF 
+  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
+  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR 
+  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
+  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT 
+  NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; 
+  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
+  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, 
+  STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF 
+  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  
+  
+  27 Oct 2018:
+  
+  U8G2_SSD1306_128X64_NONAME_1_4W_HW_SPI u8g2
+  make -f Makefile.184.uno
+  
+   text	   
+   8732	    					default, all active
+   8500	    -232	    -2.65%		no U8G2_WITH_CLIP_WINDOW_SUPPORT
+   8316	    -416	    -4.76%		no U8G2_WITH_FONT_ROTATION
+   8606	    -126	    -1.44%	 	no U8G2_WITH_UNICODE
+   8692	    -40	    -0.45%		no U8G2_WITH_INTERSECTION
+   8328	    -404	    -4.62%	  	no U8G2_WITH_INTERSECTION  no U8G2_WITH_CLIP_WINDOW_SUPPORT
+   8718	    -14	    -4.86%		no U8G2_WITH_HVLINE_SPEED_OPTIMIZATION
+   8026	    -706	    -8.08%		no U8G2_WITH_FONT_ROTATION   no U8G2_WITH_INTERSECTION  no U8G2_WITH_CLIP_WINDOW_SUPPORT
+   
+   Some flags depend on each other: `U8G2_WITH_INTERSECTION` is required for `U8G2_WITH_CLIP_WINDOW_SUPPORT`, so `U8G2_WITH_INTERSECTION` is partly active as long
+   as `U8G2_WITH_CLIP_WINDOW_SUPPORT` is requested.
+   
+*/
+#include <Arduino.h>
+const char compile_date[] = __DATE__ " " __TIME__;
+
+// 128X64G  20X4  16X2
+#define DISPLAY_TYPE '20X4'
+
+//#ifdef DISPLAY_TYPE == '20X4' || DISPLAY_TYPE == '16X2'
+//#include "Wire.h"
+//#endif
+
+#if DISPLAY_TYPE == '128X64G'
+#include <U8g2lib.h>
+#endif
+#ifdef U8X8_HAVE_HW_SPI
+#include <SPI.h>
+#endif
+#ifdef U8X8_HAVE_HW_I2C
+#include <Wire.h>
+#endif
+
+/*
+  U8g2lib Example Overview:
+    Frame Buffer Examples: clearBuffer/sendBuffer. Fast, but may not work with all Arduino boards because of RAM consumption
+    Page Buffer Examples: firstPage/nextPage. Less RAM usage, should work with all Arduino boards.
+    U8x8 Text Only Example: No RAM usage, direct communication with display controller. No graphics, 8x8 Text only.
+    
+  This is a page buffer example.    
+*/
+
+// Please UNCOMMENT one of the contructor lines below
+// U8g2 Contructor List (Picture Loop Page Buffer)
+// The complete list is available here: https://github.com/olikraus/u8g2/wiki/u8g2setupcpp
+// Please update the pin numbers according to your setup. Use U8X8_PIN_NONE if the reset pin is not connected
+// Refer to u8g2.txt to get the needed line
+#if DISPLAY_TYPE == '128X64G'
+U8G2_ST7920_128X64_1_SW_SPI u8g2(U8G2_R0, /* clock=*/ 13, /* data=*/ 11, /* CS=*/ 10, /* reset=*/ 8);
+#endif
+
+#if DISPLAY_TYPE == '20X4' || DISPLAY_TYPE == '16X2'
+#include "Wire.h"
+#include <hd44780.h> // Extensible hd44780 in library manager - https://github.com/duinoWitchery/hd44780 bperrybap - worked on Due
+#include <hd44780ioClass/hd44780_I2Cexp.h> // i2c expander i/o class header
+hd44780_I2Cexp lcd; // declare lcd object: auto locate & auto config expander chip
+// Connect to LCD via I2C, default address 0x27 (A0-A2 not jumpered)
+// LiquidCrystal_I2C lcd = LiquidCrystal_I2C(0x27, LCD_COLS, LCD_ROWS); // Change to (0x27,16,2) for 16x2 LCD.
+#endif
+
+
+void setup(void) {
+  int status;
+
+  /* U8g2 Project: SSD1306 Test Board */
+  //pinMode(10, OUTPUT);
+  //pinMode(9, OUTPUT);
+  //digitalWrite(10, 0);
+  //digitalWrite(9, 0);		
+
+  /* U8g2 Project: T6963 Test Board */
+  //pinMode(18, OUTPUT);
+  //digitalWrite(18, 1);	
+
+  /* U8g2 Project: KS0108 Test Board */
+  //pinMode(16, OUTPUT);
+  //digitalWrite(16, 0);	
+
+  /* U8g2 Project: LC7981 Test Board, connect RW to GND */
+  //pinMode(17, OUTPUT);
+  //digitalWrite(17, 0);	
+
+  /* U8g2 Project: Pax Instruments Shield: Enable Backlight */
+  //pinMode(6, OUTPUT);
+  //digitalWrite(6, 0);	
+#if DISPLAY_TYPE == '128X64G'
+  u8g2.begin();
+#endif
+#if DISPLAY_TYPE == '20X4'
+  status = lcd.begin(20, 4); // cols, rows
+#endif
+#if DISPLAY_TYPE == '16X2'
+  status = lcd.begin(16, 2); // cols, rows
+#endif
+
+}
+
+void loop(void) {
+#if DISPLAY_TYPE == '128X64G'
+  u8g2.firstPage();
+  do {
+    //u8g2.drawHLine(0,0,10);
+    //u8g2.drawHLine(0,31,10);    
+
+    u8g2.setFont(u8g2_font_ncenB10_tr);
+    u8g2.drawStr(12,20,"DinVesa.com");
+  } while ( u8g2.nextPage() );
+#endif
+#if DISPLAY_TYPE == '20X4'
+  lcd.setCursor(0, 1);
+  lcd.print(F("    DinVesa.com     "));
+#endif
+#if DISPLAY_TYPE == '16X2'
+  lcd.setCursor(0, 1);
+  lcd.print(F("  DinVesa.com   "));
+#endif
+  delay(1000);
+}
+
